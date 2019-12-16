a5112e18bf8b723aaa1cf65bbeb677248a5479f9
Karina Gaulin <karina@domandtom.com>
2019-12-16 10:47:59 -0500

    Add procfile and node version to package.json

--- src/server.js
+++ src/server.js
@@ -1,6 +1,19 @@
-const app = require('./app')
-const { PORT } = require('./config')
+// const app = require('./app')
+// const { PORT } = require('./config')
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+// app.listen(PORT, () => {
+//   console.log(`Server listening at http://localhost:${PORT}`)
+// })
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/*', (req, res) => {
+  res.json({ ok: true });
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = { app };
