26c494d617a46f1a69c663f69e597cff4776311c
Karina Gaulin <karina@domandtom.com>
2019-12-17 10:40:07 -0500

    Fix port

--- src/server.js
+++ src/server.js
@@ -1,19 +1,19 @@
-// const app = require('./app')
-// const { PORT } = require('./config')
+const app = require('./app')
+const { PORT } = require('./config')
 
-// app.listen(PORT, () => {
-//   console.log(`Server listening at http://localhost:${PORT}`)
-// })
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
 
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.get('/api/*', (req, res) => {
-  res.json({ ok: true });
-});
+// app.get('/api/*', (req, res) => {
+//   res.json({ ok: true });
+// });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-module.exports = { app };
+// module.exports = { app };
