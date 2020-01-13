eeae5c1f85d1654a286d365c3193306023615239
Karina Gaulin <karina@domandtom.com>
2020-01-13 08:47:59 -0500

    Prepare for productions

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://karinagaulin@localhost/solution-spot',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://karinagaulin@localhost/solution-spot',
+  TEST_DATABASE_URL: "postgresql://karinagaulin@localhost/solution-spot-test",
   API_BASE_URL: process.env.REACT_APP_BASE_URL || "http://localhost:3000/api"
 }
